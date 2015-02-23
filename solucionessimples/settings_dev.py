07a8462746b9405fa56f178167327ceed614a849
pabloandresvidal@gmail.com <pabloandresvidal@gmail.com>
2015-02-23 10:18:07 -0500

    Se borraron los archivos duplicados de media y funciona. Se adiciono el componenete para produccion de settings

---
+++ solucionessimples/settings_dev.py
@@ -0,0 +1,53 @@
+
+from .settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+########## MEDIA CONFIGURATION
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = normpath(join(DJANGO_ROOT, 'media'))
+
+# URL that handles the media served from MEDIA_ROOT.
+MEDIA_URL = '/media/'
+########## END MEDIA CONFIGURATION
+
+
+########## STATIC FILE CONFIGURATION
+# Absolute path to the directory static files should be collected to. Don't put
+# anything in this directory yourself; store your static files in apps' static/
+# subdirectories and in STATICFILES_DIRS.
+STATIC_ROOT = '/home/pablovidal/webapps/static'
+
+# URL prefix for static files.
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files.
+STATICFILES_DIRS = (
+    normpath(join(DJANGO_ROOT, 'static')),
+)
+
+ROOT_URLCONF = 'solucionessimples.urls'
+
+WSGI_APPLICATION = 'solucionessimples.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
+}
